4557ce371e3cddb504e4e232d1231c7204f292b8
Marcel_PC <liemmina24@gmail.com>
2024-12-03 00:38:35 +0700

    Main

---
+++ src/main/java/com/example/marcjavafundamental/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.marcjavafundamental;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Tessy");
+        System.out.print("Wahyuni");
+        System.out.print("Riwayati");
+        System.out.print("Hartati");
+    }
+}
